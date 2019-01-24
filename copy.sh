db9e7678529126a5ecb3d4efe43fe18a26fa3e61
chonlatit (Karn) <31027816+guitarlnw@users.noreply.github.com>
2019-01-24 13:16:22 +0700

    Delete copy.sh

--- copy.sh
+++
@@ -1,7 +0,0 @@
-GITHUB_REPO="/Users/chonlatit/work/thinknetmaps"
-GITLAB_REPO="/Users/chonlatit/work/mapbox-gl-js"
-
-echo Cloning mapbox-gl-js [GITLAB] to thinknetmaps [GITHUB]...
-rm -rf $GITHUB_REPO/*
-rsync --progress -r --exclude=node_modules $GITLAB_REPO/* $GITHUB_REPO
-echo Your project has been cloned, please commit and push new vesion of THiNKNET Map
